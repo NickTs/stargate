f9dfefdf01579b8fa033b3ae57ee5431b45bddbc
tsuprick <tsuprick@gmail.com>
2019-09-26 15:07:50 +0300

    create component

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {Component} from 'react'
+import Header from './src/component/uikit/Header'
 
 class App extends Component {
+  state = {
+    title: 'Wallet exchange of values',
+  }
+
   render() {
-    return (
-      <View style={styles.cont}>
-        <Text style={styles.welcome}>Hello Nick</Text>
-      </View>
-    );
+    return <Header title={this.state.title} />
   }
 }
 
-const styles = StyleSheet.create({
-  cont: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 24,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-export default App;
+export default App
---
+++ src/component/uikit/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Text, StyleSheet, View} from 'react-native'
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: '#1b5193',
+    height: 116,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 28,
+    fontFamily: 'AvenirNext-DemiBold',
+    textAlign: 'center',
+    // margin: 10,
+  },
+})
+
+export default Header
